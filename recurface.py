e433fe2621e275c649178d3b3ad3546f91193e10
Immijimmi <immijimmi1@gmail.com>
2021-01-11 21:07:58 +0000

    Added more docstrings, set ._reset to private

--- recurface.py
+++ recurface.py
@@ -7,9 +7,9 @@ from typing import Iterable, Tuple, Optional
 class Recurface:
     """A pygame framework used to organise Surfaces into a chain structure"""
 
-    def __init__(self, surface: Surface, position: Iterable[int]):
+    def __init__(self, surface: Surface, position: Optional[Iterable[int]] = None):
         self.__surface = surface  # Should hold a pygame Surface
-        self.__position = list(position)  # (x, y) position to blit to in the containing Surface
+        self.__position = list(position) if position else None  # (x, y) position to blit to in the containing Surface
 
         self.__parent = None
         self.__children = set()
@@ -28,29 +28,43 @@ class Recurface:
         self.__surface = value
 
     @property
-    def position(self) -> Tuple[int]:
-        return tuple(self.__position)
+    def position(self) -> Optional[Tuple[int]]:
+        return tuple(self.__position) if self.__position else None
 
     @position.setter
-    def position(self, value: Iterable[int]):
-        self.__rect__previous = self.__rect
-        self.__position = list(value)
+    def position(self, value: Optional[Iterable[int]]):
+        if self.__position:
+            self.__rect__previous = self.__rect
+
+        self.__position = list(value) if value else None
 
     @property
     def x(self) -> int:
+        if not self.__position:
+            raise ValueError("position is not currently set")
+
         return self.__position[0]
 
     @x.setter
     def x(self, value: int):
+        if not self.__position:
+            raise ValueError("position is not currently set")
+
         self.__rect__previous = self.__rect
         self.__position[0] = value
 
     @property
     def y(self) -> int:
+        if not self.__position:
+            raise ValueError("position is not currently set")
+
         return self.__position[1]
 
     @y.setter
     def y(self, value: int):
+        if not self.__position:
+            raise ValueError("position is not currently set")
+
         self.__rect__previous = self.__rect
         self.__position[1] = value
 
@@ -59,7 +73,7 @@ class Recurface:
         return self.__parent
 
     @parent.setter
-    def parent(self, value: "Recurface"):
+    def parent(self, value: Optional["Recurface"]):
         if self.__parent:
             if self.__parent is value:
                 return  # Parent is already correctly set
@@ -80,16 +94,46 @@ class Recurface:
 
         self.__children.add(child)
         child.parent = self
-        child.reset()
+        child._reset()
 
     def remove_child(self, child: "Recurface") -> None:
         self.__children.remove(child)
         child.parent = None
 
-    def render(self, destination: Surface) -> Iterable[Rect]:
-        result = []
+    def add_update_rects(self, rects: Iterable[Optional[Rect]], update_position: bool = False) -> None:
+        """
+        Stores provided rects to be updated on the next render.
+        If update_position is True, the rects are offset by self.position before storing
+        """
 
-        for child in self.__children:
+        if update_position:
+            if not self.__position:
+                raise ValueError("position is not currently set")
+
+            for rect in rects:
+                rect.x += self.__position[0]
+                rect.y += self.__position[1]
+
+        self.__rect__additional += rects
+
+    def render(self, destination: Surface) -> Iterable[Optional[Rect]]:
+        """
+        Blits surfaces to the provided destination for this object and any of its children.
+        This function should be called on top-level (parent-less) recurfaces once per game tick, and
+        pygame.display.update() should be passed all returned rects
+        """
+
+        result = []
+        is_rendered = bool(self.__rect)  # Surface has been rendered previously
+
+        if not self.__position:  # If position is None, nothing should display to the screen
+            if is_rendered:  # If something was previously rendered, that area of the screen needs updating to remove it
+                result.append(self.__rect)
+                self.__rect = None  # is_rendered will now be False on the next .render call
+
+            return result
+
+        for child in self.__children:  # Render all child objects and collect returned Rects
             rects = child.render(self.__surface)
 
             for rect in rects:
@@ -99,29 +143,53 @@ class Recurface:
 
                     result.append(rect)
 
-        is_rendered = bool(self.__rect)
         self.__rect = destination.blit(self.__surface, self.__position)
-        if not is_rendered:
-            result.append(self.__rect)  # On the first render, return the full surface
-            return result
 
-        if self.__rect__previous:
+        if not is_rendered:  # On the first render, return the full surface
+            return [self.__rect]
+
+        if self.__rect__previous:  # If the surface was changed or moved
             result += [self.__rect__previous, self.__rect]
             self.__rect__previous = None
 
-        if self.__rect__additional:
+        if self.__rect__additional:  # If there are any extra areas that need updating
             result += self.__rect__additional
             self.__rect__additional = []
 
         return result
 
-    def reset(self) -> None:
+    def unlink(self) -> None:
+        """
+        Removes this object from its place in the chain,
+        attaching any child objects to this object's parent if there is one
+        """
+
+        for child in self.children:
+            child.parent = self.__parent
+
+        self.parent = None
+
+    def _reset(self) -> None:
+        """
+        Sets variables which hold the object's rendering details back to their default values.
+        This should only be done if the parent object is being changed
+        """
+
         self.__rect = None
         self.__rect__previous = None
         self.__rect__additional = []
 
     def __del__(self):
+        """
+        Note that deleting this object will leave any child objects with no parent object.
+        Calling .unlink() before deleting will attach child objects to this object's parent if there is one
+        """
+
         for child in self.__children:
             self.remove_child(child)
 
-        self.parent = None
+        if self.__parent:
+            self.__parent.add_update_rects([self.__rect, self.__rect__previous, *self.__rect__additional])
+            self._reset()
+
+            self.parent = None
