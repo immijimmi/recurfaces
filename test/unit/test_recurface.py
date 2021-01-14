86d15726134ab577af82a57c40753bffc1574dfa
Immijimmi <immijimmi1@gmail.com>
2021-01-14 12:47:43 +0000

    Added tests, bat file to run all

--- test/unit/test_recurface.py
+++ test/unit/test_recurface.py
@@ -4,15 +4,33 @@ from ...recurface import Recurface
 
 
 def test_no_position_on_first_render_returns_no_rects():
-    surf_bg = Surface((800, 600))
-    surf_1 = Surface((300, 300))
-    surf_2 = Surface((100, 100))
+    surface_bg = Surface((800, 600))
+    surface_1 = Surface((300, 300))
 
-    recurface = Recurface(surf_1, (0, 0))
-    recurface.position = (0, 0)
-    recurface.surface = surf_2
-    recurface.position = None
-
-    rects = recurface.render(surf_bg)
+    recurface = Recurface(surface_1)
+    rects = recurface.render(surface_bg)
 
     assert rects == []
+
+
+def test_first_valid_render_returns_correct_rect():
+    surface_bg = Surface((800, 600))
+    surface_1 = Surface((300, 300))
+
+    recurface = Recurface(surface_1, (0, 0))
+    rects = recurface.render(surface_bg)
+
+    assert rects == [surface_1.get_rect()]
+
+
+def test_no_position_once_rendered_returns_previous_rect():  #####
+    surface_bg = Surface((800, 600))
+    surface_1 = Surface((300, 300))
+
+    recurface = Recurface(surface_1, (0, 0))
+    recurface.render(surface_bg)
+
+    recurface.position = None
+    rects = recurface.render(surface_bg)
+
+    assert rects == [surface_1.get_rect()]
