25684839a13b4c1513f73e38849cce3ee1c3077d
immijimmi <immijimmi1@gmail.com>
2021-11-04 23:27:03 +0000

    Updated setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     packages=[
         "recurfaces"
     ],
-    version="0.4.0",
+    version="1.0.0",
     license="LGPLv3",
     description="A pygame framework used to organise Surfaces into a chain structure",
     long_description_content_type="text/markdown",
@@ -16,7 +16,7 @@ setup(
     author="immijimmi",
     author_email="imranhamid99@msn.com",
     url="https://github.com/immijimmi/recurfaces",
-    download_url="https://github.com/immijimmi/recurfaces/archive/refs/tags/v0.4.0.tar.gz",
+    download_url="https://github.com/immijimmi/recurfaces/archive/refs/tags/v1.0.0.tar.gz",
     keywords=["ui", "gui", "graphical", "user", "interface", "game"],
     install_requires=[
         "pygame~=2.0.1"
