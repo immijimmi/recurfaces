0924c7a5aeda7e00465af21d63cd6bc8f19fc5d1
immijimmi <imranhamid99@msn.com>
2022-07-14 09:49:34 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -26,6 +26,6 @@ setup(
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Libraries :: Application Frameworks",
         "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.8",
     ],
 )
